9eb5a258b50e2a20af839111f72f3f117fe915fe
Mário Gomes <marioferreiragomes333@gmail.com>
2024-05-05 20:17:28 +0100

    test: ensure SingUpController returns 400 if email is not provided

--- src/presentation/controller/singup.spec.ts
+++ src/presentation/controller/singup.spec.ts
@@ -4,7 +4,6 @@ describe('SingUpController', () => {
     const sut = new SingUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         confirmPassword: 'any_password'
@@ -15,5 +14,21 @@ describe('SingUpController', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('name is missing'))
+  })
+
+  test('should return 400 if email is not provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        confirmPassword: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('email is missing'))
   })
 })
