c1eb24a8aa422a6fe35cc57bc7adb65a0b57b771
Mário Gomes <marioferreiragomes333@gmail.com>
2024-05-06 22:36:23 +0100

    refactor:add error helper in singup method

--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -1,21 +1,12 @@
 import { type HttpRequest, type HttpResponse } from '../../protocols/http'
 import { MissingParamError } from '../errors/MissingParam'
+import { badRequest } from '../helpers/http-helper'
 
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
-    }
+    if (!httpRequest.body.name) return badRequest(new MissingParamError('name'))
 
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
-    }
+    if (!httpRequest.body.email) return badRequest(new MissingParamError('email'))
 
     return {
       statusCode: 200,
