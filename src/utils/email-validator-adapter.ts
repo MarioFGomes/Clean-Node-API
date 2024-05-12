589b249e161aa4bbf7a58b5292677757b0558055
Mário Gomes <marioferreiragomes333@gmail.com>
2024-05-12 21:00:56 +0100

    feature:ensure Email validator Adapter return true if validator return true

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,8 @@
 import { type IEmailValidator } from '../protocols/email-validator'
+import validator from 'validator'
 
 export class EmailValidatorAdapter implements IEmailValidator {
   isValid (email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
