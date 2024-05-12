a65e818760f145e8b0619faf5f731e26558e398c
Mário Gomes <marioferreiragomes333@gmail.com>
2024-05-12 21:01:18 +0100

    test:ensure Email validator Adapter return true if validator return true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,22 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
 
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 describe('Email validator Adapter', () => {
   test('should return false if email is invalid', () => {
     const sut = new EmailValidatorAdapter()
-    const valid = sut.isValid('Invalid_email@mail.com')
-    expect(valid).toBe(false)
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const IsValid = sut.isValid('Invalid_email@mail.com')
+    expect(IsValid).toBe(false)
+  })
+
+  test('should return true if email is valid', () => {
+    const sut = new EmailValidatorAdapter()
+    const valid = sut.isValid('valid_email@mail.com')
+    expect(valid).toBe(true)
   })
 })
