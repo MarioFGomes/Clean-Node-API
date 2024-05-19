b23ab09dc0821e016bafea6319b122d9d875e8b0
Mário Gomes <marioferreiragomes333@gmail.com>
2024-05-19 20:35:39 +0100

    refactor:move create sut for the single class

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,15 +1,29 @@
 import { type Encrypter } from '../../protocols/encrypter'
 import { DbAccount } from './db-add-account'
 
+interface SutTypes {
+  sut: DbAccount
+  encryptStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncryptStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => { resolve('hashed_password') })
+    }
+  }
+
+  const encryptStub = new EncryptStub()
+  const sut = new DbAccount(encryptStub)
+
+  return {
+    sut,
+    encryptStub
+  }
+}
 describe('DbAccount UseCases', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncryptStub implements Encrypter {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => { resolve('hashed_password') })
-      }
-    }
-    const encryptStub = new EncryptStub()
-    const sut = new DbAccount(encryptStub)
+    const { sut, encryptStub } = makeSut()
     const EncryptSpy = jest.spyOn(encryptStub, 'encrypt')
     const AccountData = {
       name: 'valid_name',
