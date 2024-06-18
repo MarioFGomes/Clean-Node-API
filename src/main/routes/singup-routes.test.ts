171ae6e9782e3acb5371ffe77e85be972f7d9592
Mário Gomes <marioferreiragomes333@gmail.com>
2024-06-18 23:22:17 +0100

    refactor:connect to mongobd before integration test

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -1,9 +1,17 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SingUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('should return an account on success', async () => {
-    await request(app).post('/api/singup')
+    await request(app).post('/api/signup')
       .send({
         name: 'Mario Gomes',
         email: 'mariogomes@gmail.com',
--- src/main/routes/singup.routes.ts
+++ src/main/routes/singup.routes.ts
@@ -1,7 +1,7 @@
 import { type Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/singup', (req, res) => {
+  router.post('/signup', (req, res) => {
     res.json({ ok: 'ok' })
   })
 }
