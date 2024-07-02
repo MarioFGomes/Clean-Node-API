c9d09de0124dc82b6cd2a801675c74f9a4b58afc
Mário Gomes <marioferreiragomes333@gmail.com>
2024-07-02 04:57:36 +0100

    feat:ensure LogControllerDecorator calls LogErrorRepository if controller return a server error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,12 +1,16 @@
 import { type HttpRequest, type HttpResponse, type IController } from '../../protocols'
+import { type ILogErrorRepository } from '../../data/protocols/log-error-repository'
 
 export class LogControllerDecorator implements IController {
-  constructor (private readonly _controller: IController) {}
+  constructor (private readonly _controller: IController,
+    private readonly logErrorRepository: ILogErrorRepository
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this._controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) {
-      // log
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      await this.logErrorRepository.log(httpResponse.body.stack)
     }
     return httpResponse
   }
