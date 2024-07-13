b95da905277350a9b9ace05c49a9d4546265a953
Mário Gomes <marioferreiragomes333@gmail.com>
2024-07-13 12:56:51 +0100

    feat:ensure login controller return 500 if email validator throw

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -1,27 +1,32 @@
 import { type HttpRequest, type HttpResponse, type IController } from '../../../protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { BadRequest } from '../../helpers/http-helper'
+import { BadRequest, serverError } from '../../helpers/http-helper'
 import { type IEmailValidator } from '../singup/singup-protocols'
 
 export class LoginController implements IController {
   constructor (private readonly emailValidator: IEmailValidator) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return await new Promise(resolve => { resolve(BadRequest(new MissingParamError('email'))) })
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return await new Promise(resolve => { resolve(BadRequest(new MissingParamError('email'))) })
+      }
+      if (!password) {
+        return await new Promise(resolve => { resolve(BadRequest(new MissingParamError('password'))) })
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const IsValid = this.emailValidator.isValid(email)
+      if (!IsValid) {
+        return await new Promise(resolve => { resolve(BadRequest(new InvalidParamError('email'))) })
+      }
+      const data = {
+        statusCode: 200,
+        body: 'anything'
+      }
+      return await new Promise(resolve => { resolve(data) })
+    } catch (err: any) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      return serverError(err)
     }
-    if (!password) {
-      return await new Promise(resolve => { resolve(BadRequest(new MissingParamError('password'))) })
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const IsValid = this.emailValidator.isValid(email)
-    if (!IsValid) {
-      return await new Promise(resolve => { resolve(BadRequest(new InvalidParamError('email'))) })
-    }
-    const data = {
-      statusCode: 200,
-      body: 'anything'
-    }
-    return await new Promise(resolve => { resolve(data) })
   }
 }
