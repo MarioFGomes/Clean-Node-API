6497369004fe7be49482b5a22a0b585882483f8a
Mário Gomes <marioferreiragomes333@gmail.com>
2024-12-09 23:25:07 +0100

    feat:ensure DBauthentication call HashComparer with correct value

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,10 +1,12 @@
 import { type AuthenticationModel, type IAuthentication } from '../../../domain/usecases/authentication'
+import { type IHashComparer } from '../../protocols/criptography/hashe-compare'
 import { type loadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DBauthentication implements IAuthentication {
-  constructor (private readonly loadAccountByEmailRepository: loadAccountByEmailRepository) {}
+  constructor (private readonly loadAccountByEmailRepository: loadAccountByEmailRepository, private readonly hashComparer: IHashComparer) {}
   async auth (authenticator: AuthenticationModel): Promise<string | null> {
-    await this.loadAccountByEmailRepository.load(authenticator.email)
+    const account = await this.loadAccountByEmailRepository.load(authenticator.email)
+    if (account) await this.hashComparer.compare(authenticator.password, account.password)
     return null
   }
 }
