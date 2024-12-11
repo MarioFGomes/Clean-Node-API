3c24c381a6ecf5e3045088977270a8384dc4f89c
Mário Gomes <marioferreiragomes333@gmail.com>
2024-12-11 21:01:51 +0100

    feat:DbAuthentication call token generator with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,12 +1,16 @@
 import { type AuthenticationModel, type IAuthentication } from '../../../domain/usecases/authentication'
 import { type IHashComparer } from '../../protocols/criptography/hashe-compare'
+import { type ITokenGenerator } from '../../protocols/criptography/token-generator'
 import { type loadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DBauthentication implements IAuthentication {
-  constructor (private readonly loadAccountByEmailRepository: loadAccountByEmailRepository, private readonly hashComparer: IHashComparer) {}
+  constructor (private readonly loadAccountByEmailRepository: loadAccountByEmailRepository, private readonly hashComparer: IHashComparer, private readonly tokenGenerator: ITokenGenerator) {}
   async auth (authenticator: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authenticator.email)
-    if (account) await this.hashComparer.compare(authenticator.password, account.password)
+    if (account) {
+      await this.hashComparer.compare(authenticator.password, account.password)
+      await this.tokenGenerator.generate(account.id)
+    }
     return null
   }
 }
