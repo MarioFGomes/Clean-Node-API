c97aec03ad3bc5e07624f9e4345c7f129688efa7
Mário Gomes <marioferreiragomes333@gmail.com>
2024-12-16 22:55:59 +0100

    refactor:singnup factory

--- src/main/factories/singnup/signup.ts
+++ src/main/factories/singnup/signup.ts
@@ -1,6 +1,6 @@
 import { SingUpController } from '../../../presentation/controller/singup/singup'
 import { DbAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogControllerDecorator } from '../../decorators/log'
 import { type IController } from '../../../protocols'
